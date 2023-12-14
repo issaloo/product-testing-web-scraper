243753bb13dc3dce22d7dde83588262cdfc64a43
Issac Loo <issaloo@umich.edu>
2023-12-14 16:27:39 -0500

    feat(main): update (#5)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process, Queue
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -6,19 +8,36 @@ from scraper.spiders.main_spider import MainSpider
 
 def product_testing_web_scraper(event, context):
     """Cloud Function entry point function."""
-    settings = get_project_settings()
-    settings.setdict(
-        {
-            "LOG_LEVEL": "ERROR",
-            "LOG_ENABLED": True,
-        }
-    )
 
-    process = CrawlerProcess(settings)
-    process.crawl(MainSpider)
-    process.start()
+    def script(queue):
+        try:
+            settings = get_project_settings()
 
-    return "Run Successful!"
+            settings.setdict(
+                {
+                    "LOG_LEVEL": "ERROR",
+                    "LOG_ENABLED": True,
+                }
+            )
+
+            process = CrawlerProcess(settings)
+            process.crawl(MainSpider)
+            process.start()
+            queue.put(None)
+        except Exception as e:
+            queue.put(e)
+
+    queue = Queue()
+
+    # Wrap the spider in a child process
+    main_process = Process(target=script, args=(queue,))
+    main_process.start()
+    main_process.join()
+
+    result = queue.get()
+    if result is not None:
+        raise result
+    return "ok"
 
 
 # TODO: Uncomment for Local Development, o/w keep commented
