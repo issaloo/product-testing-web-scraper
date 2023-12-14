22949fb51228007afa63aafb27d43a7403048833
Issac Loo <issaloo@umich.edu>
2023-12-13 23:54:09 -0500

    feat(multiple): pipelines

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -10,10 +10,15 @@ from sendgrid.helpers.mail import Content, Email, Mail, To
 load_dotenv()
 
 # TODO: Uncomment for Local Development, o/w keep commented
-SITE_PRODUCT_URL = os.getenv("SITE_PRODUCT_URL")
+SITE_URL = os.getenv("SITE_URL")
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL")
 SENDGRID_TO_EMAIL = os.getenv("SENDGRID_TO_EMAIL")
+
+SITE_PRODUCT_URL = f"{SITE_URL}/product-tests"
+
+# TODO: Uncomment for Production, o/w keep commented
+# TODO: add Env variables here
 
 
 class PrepareProductContentPipeline:
@@ -34,15 +39,15 @@ class PrepareProductContentPipeline:
             scrapy.Item: The processed item if all required fields are present.
         """
         adapter = ItemAdapter(item)
-
         product_api_list = adapter.get("product_api_list")
         is_product_on_page = adapter.get("is_product_on_page")
+
         product_api_list = sorted(product_api_list, key=lambda tup: tup[1], reverse=True)
         adapter["product_api_list"] = product_api_list
         product_api_content = [
             f"Name: {p_info[0]}\n Price: {p_info[1]}\n Link:{p_info[2]}" for p_info in product_api_list
         ]
-        product_email_content = ["{SITE_PRODUCT_URL}\n\n"]
+        product_email_content = [f"{SITE_PRODUCT_URL}"]
         if product_api_list:
             product_email_content.extend(product_api_content)
         if is_product_on_page:
@@ -67,7 +72,7 @@ class SendEmailPipeline:
         adapter = ItemAdapter(item)
         product_api_list = adapter.get("product_api_list")
         is_product_on_page = adapter.get("is_product_on_page")
-        product_api_content = adapter.get("product_content")
+        product_email_content = adapter.get("product_email_content")
 
         # set up emails
         sg = SendGridAPIClient(api_key=SENDGRID_API_KEY)
@@ -77,46 +82,13 @@ class SendEmailPipeline:
 
         # send email
         if product_api_list or is_product_on_page:
-            content = Content("text/plain", product_api_content)
+            content = Content("text/plain", product_email_content)
             mail = Mail(from_email, to_email, subject, content)
             mail_json = mail.get()
             sg.client.mail.send.post(request_body=mail_json)
-            yield Request(url=product_api_list[0][2])
-        return None
-
-    # ADD BELOW TO THE Pipelines for after getting necessary data Item
-    # def send_email(self, response):
-    #          # set sendgrid variables
-    #     sg = sendgrid.SendGridAPIClient(api_key=email_api_key)
-    #     from_email = Email("digestiblecontents@gmail.com")
-    #     to_email = To("issactranloo@gmail.com")
-    #     subject = "Products Available!"
-    #     if email_product_list:
-    #         email_product_list = sorted(email_product_list, key=lambda tup: tup[1], reverse=True)
-    #         email_product_content = [f"Name: {p_info[0]}\n Price: {p_info[1]}\n Link:{p_info[2]}" for p_info in email_product_list]
-    #         email_product_content = f"{SITE_PRODUCT_URL}\n\n" + '\n\n'.join(email_product_content)
-
-    #         # send email
-    #         content = Content("text/plain", email_product_content)
-    #         mail = Mail(from_email, to_email, subject, content)
-    #         mail_json = mail.get()
-    #         response = sg.client.mail.send.post(request_body=mail_json)
-
-    #         # click on the first product link
-    #         first_product_response = sess.get(email_product_list[0][2])
-    #            else:
-    #                 product_main_response = sess.get(product_main_url)
-    #                 product_main_soup = BeautifulSoup(product_main_response.text, "html.parser")
-    #                 see_job = product_main_soup.find(text="See Job Details")
-    #                 see_product = product_main_soup.find(text="See Product Details")
-    #                 if see_job or see_product:
-    #                      # send email
-    #                      email_product_content = f"{product_main_url}\n\n There is a potential Job!"
-    #                      content = Content("text/plain", email_product_content)
-    #                      mail = Mail(from_email, to_email, subject, content)
-    #                      mail_json = mail.get()
-    #                      response = sg.client.mail.send.post(request_body=mail_json)
-    #                 else:
-    #                      print("No products currently available.")
-    #       else:
-    #            print("Reached max product testing threshold, no products will show.")
+            Request(url=product_api_list[0][2])
+        response = Request(url=SITE_PRODUCT_URL)
+        # TODO: add credentials at this step.. how to do that?
+        # TODO: maybe do a callback?
+        return response.body
+        # return None
--- scraper/settings.py
+++ scraper/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "scraper.pipelines.ScraperPipeline": 300,
-# }
+ITEM_PIPELINES = {
+    "scraper.pipelines.PrepareProductContentPipeline": 300,
+    "scraper.pipelines.SendEmailPipeline": 301,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
